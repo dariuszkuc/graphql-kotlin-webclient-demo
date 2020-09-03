d2d857e5d131f12b048febd4e37241643a612886
Dariusz Kuc <dkuc@expedia.com>
2020-09-02 23:59:29 -0500

    basic client usage

--- src/main/kotlin/com/expediagroup/graphql/client/Application.kt
+++ src/main/kotlin/com/expediagroup/graphql/client/Application.kt
@@ -1,5 +1,22 @@
 package com.expediagroup.graphql.client
 
+import com.expediagroup.graphql.generated.ConferenceQuery
+import com.expediagroup.graphql.generated.GreetingsQuery
+import kotlinx.coroutines.runBlocking
+
 fun main() {
-    println("Hello World!!!")
+    runBlocking {
+        val client = GraphQLWebClient(url = "http://localhost:8080/graphql")
+
+        val greetingsQuery = GreetingsQuery(client)
+        val greeting = greetingsQuery.execute()
+        println(greeting.data?.schedule)
+        println("---")
+
+        val conferenceQuery = ConferenceQuery(client)
+        val result = conferenceQuery.execute(variables = ConferenceQuery.Variables())
+        println(result.data?.conference)
+        println(result.data?.schedule)
+        println(result.data?.people)
+    }
 }
