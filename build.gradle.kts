3d0f390f35eaa8f0c5fb8597317812e9ff71a5fb
Dariusz Kuc <dkuc@expedia.com>
2020-09-02 23:54:57 -0500

    configure build plugin and add query files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+import com.expediagroup.graphql.plugin.gradle.graphql
+
 plugins {
     application
     id("org.jetbrains.kotlin.jvm") version "1.3.72"
+    id("com.expediagroup.graphql") version "4.0.0-alpha.1"
 }
 
 repositories {
@@ -11,6 +14,7 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.3.72")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.3.72")
+    implementation("com.expediagroup:graphql-kotlin-spring-client:4.0.0-alpha.1")
 }
 
 application {
@@ -20,5 +24,12 @@ application {
 tasks {
     compileKotlin {
         kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
+graphql {
+    client {
+        endpoint = "http://localhost:8080/graphql"
+        packageName = "com.expediagroup.graphql.generated"
     }
 }
